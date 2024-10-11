12704599ddf43d77b29db9792980645cf39eb682
YoussefKamal098 <kmalywsf098@gmail.com>
2024-10-11 17:34:06 +0300

    101-safely_get_value.py, 102-type_checking.py

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+This module provides a utility function to safely retrieve values from
+a mapping (like a dictionary). The function allows specifying a
+default value to return in case the requested key is not found in
+the mapping. The module also uses Python's type hinting and
+generics (`TypeVar`) to ensure flexibility
+and consistency in return types.
+"""
+
+from typing import TypeVar, Mapping, Any, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping[Any, Any], key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    Safely retrieves a value from a dictionary-like mapping,
+    with an optional default value.
+
+    This function attempts to retrieve the value associated with a
+    given key from the provided mapping (e.g., dictionary).
+    If the key exists, it returns the corresponding value. Otherwise,
+    it returns the default value provided,
+    or `None` if no default is specified.
+
+    Args:
+        dct (Mapping[Any, Any]): The mapping (dictionary-like object)
+        to search for the key.
+        key (Any): The key to search for in the mapping.
+        default (Union[T, None], optional): The value to return if the
+        key is not found. Defaults to `None`.
+
+    Returns:
+        Union[Any, T]: The value corresponding to the key,
+        or the `default` value if the key is not present.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""
+This module provides a utility function to 'zoom' into a tuple of
+integers by repeating each element a specified number of times.
+The `zoom_array` function allows scaling of the tuple elements by a
+given factor, and returns the zoomed version as a list.
+"""
+
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+    """
+    Repeats each element of a tuple a specified number of times.
+
+    This function takes a tuple of integers and
+    repeats each element by the specified `factor`.
+    The result is a list with each element repeated consecutively.
+
+    Args:
+        lst (Tuple[int, ...]): A tuple of integers to be zoomed in on.
+        factor (int, optional): The number of times each element
+        should be repeated. Defaults to 2.
+
+    Returns:
+        List[int]: A list containing the repeated elements of the tuple.
+    """
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = tuple([12, 72, 91])
+
+zoom_2x = zoom_array(array)
+zoom_3x = zoom_array(array, int(3.0))
